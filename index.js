8429a5bbcdddbf277834ebfc4a36caf2a68c981e
Jonathon Murphy <jsmurphy1991@gmail.com>
2019-10-31 19:21:48 -0400

    Removing the CustomReport class, and re-integrating it into the StudentObject class

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const fs = require('fs'),
       crr = require('./lib/custom-reading-reports'),
       // Name of the school or district that the report is for
       schoolName = '',
-      // Enter all Inst SID's that are being used to pull the report 
+      // Enter all Inst SID's that are being used to pull the report
       instIDArray = [],
       options = {fields: [
           {name: 'inst', label: 'Institution'},
           {name: 'className', label: 'Class Name'},
+          {name: 'primaryTeacher', label: 'Primary Teacher of Class'},
           {name: 'fullName', label: 'Students Name'},
           {name: 'gradeLevel', label: 'Grade Level'},
-          {name: 'timePlayed', label: 'Time Played since October 2, 2019'},
-          {name: 'primaryTeacher', label: 'Primary Teacher of Class'}
+          {name: 'timePlayed', label: 'Time Played since October 2, 2019'}
         ]};
 
 // These will all be left empty
 let obj, res, classe, student;
 let classArray = [];
-let customReport = [];
+let studentArray = [];
 
 let timeSpentPostData = {
   StudentIds : [],
@@ -28,34 +28,36 @@ let timeSpentPostData = {
 };
 
 (async () => {
+  // Pull class data from admin and make a new object with it
+  // Does not pull any student data
   let classDataRes = await crr.getClassData(instIDArray);
   for (classe of classDataRes.classes) {
     let tempObj = new crr.ClassObject(classe);
     classArray.push(tempObj);
   };
-  for (classe of classArray) {
-    let classStateRes = await crr.getClassState(classe.classID);
+  // Get the student data from the Aqua servers using the class eid's
+  for (classObj of classArray) {
+    let classStateRes = await crr.getClassState(classObj.classID);
     for (student of classStateRes) {
-      let tempObj = new crr.StudentObject(student);
-      classe.students.push(tempObj);
+      let tempObj = new crr.StudentObject(student, classObj);
+      studentArray.push(tempObj);
       timeSpentPostData.StudentIds.push(tempObj.aquaID);
     };
   };
+  // Pull the usage time for all the students and
+  // merge the two arrays (timeSpentRes and studentArray)
+  // together
   let timeSpentRes = await crr.getUsageStats(timeSpentPostData);
   for (res of timeSpentRes) {
-    for (classe of classArray) {
-      for (student of classe.students) {
-        if (res.user_id == student.aquaID) {
-          student.timePlayed = crr.secondsToHoursAndMinutes(res.duration);
-          // Re-organize into a structure parsable by json-csv
-          let tempObj = new crr.CustomReport(classe, student);
-          customReport.push(tempObj);
-        };
+    for (student of studentArray) {
+      if (res.user_id == student.aquaID) {
+        student.timePlayed = crr.secondsToHoursAndMinutes(res.duration);
       };
     };
   };
+
   let out = fs.createWriteStream(`./reports/${schoolName}CustomReport.csv`, {encoding: 'utf8'});
-  let readable = es.readArray(customReport);
+  let readable = es.readArray(studentArray);
   readable
     .pipe(jsoncsv.csv(options))
     .pipe(out);
